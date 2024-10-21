cde1e7478377842a6a96e04e019107fd1f115f87
weiyu <15152673673@163.com>
2024-10-22 00:04:19 +0800

    初始化项目

---
+++ src/main/java/com/weiyu/easycarpooluser/EasyCarPoolUserApplication.java
@@ -0,0 +1,13 @@
+package com.weiyu.easycarpooluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCarPoolUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCarPoolUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weiyu/easycarpooluser/EasyCarPoolUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiyu.easycarpooluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCarPoolUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
